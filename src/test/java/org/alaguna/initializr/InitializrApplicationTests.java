ba3505893e49f1faa48d345ed5b29cd729d2b98d
Alberto Laguna <albertolh3@gmail.com>
2020-12-09 19:28:47 +0100

    init repository

---
+++ src/test/java/org/alaguna/initializr/InitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alaguna.initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
